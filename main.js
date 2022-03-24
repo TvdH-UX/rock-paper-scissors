b7e4ff76da978fa7cdcaa86bb90c0ef34e05edc9
TvdH-UX <thomasheuvel7@gmail.com>
2022-03-24 13:56:30 +0100

    Add function to play a single round

--- main.js
+++ main.js
@@ -51,6 +51,22 @@ const playerSelection = playerInput();
 
 function playRound(playerSelection, computerSelection){
     console.log(playerSelection);
-}
+    console.log(computerSelection);
+    if (playerSelection === computerSelection){
+        return "It's a tie!";
+    } else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock"){
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You Win! Scissors beats Paper";
+    };
+};
 
 console.log(playRound(playerSelection, computerSelection));
